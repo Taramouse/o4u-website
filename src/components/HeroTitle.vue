4988ae18d585e7e041bc5e9d76cb51a71db96e14
taramouse <nickykeyse@gmail.com>
2018-09-28 12:08:41 +0100

    Hero component added.

---
+++ src/components/HeroTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="hero" :class="[colorClass, sizeClass]">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            {{ heroMainTitle }}
+          </h1>
+          <h2 class="subtitle">
+            {{ heroSubtitle }}
+          </h2>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'HeroTitle',
+  props: ['colorClass', 'sizeClass', 'heroMainTitle', 'heroSubtitle']
+}
+</script>
